2c4ebc3529b7f0d46fcfbc21d033b1d52962011b
Gaurank <gaurankmauryaaonla@gamil.com>
2024-05-07 01:16:15 +0530

    commit

---
+++ calculator.js
@@ -0,0 +1,40 @@
+let string = "";
+var display = document.querySelector('.display');
+let buttons = document.querySelectorAll(".enter");
+let ON = document.querySelector('.onn')
+let OFF = document.querySelector('.off')
+
+ON.addEventListener('click', function () {
+    OFF.style.display = "flex"
+    Array.from(buttons).forEach(function(value){
+        value.style.display = "flex"
+    })
+});
+
+Array.from(buttons).forEach(function (value) {
+    value.addEventListener('click', function (event) {
+        if (value.innerHTML == '=') {
+            string = eval(string);
+            display.innerHTML = `${string}`
+        }
+        // else if (value.innerHTML == 'OFF') {
+        //     value.style.display = "none"
+        // }
+        else if(value.innerHTML == "AC"){
+            string = "";
+            display.innerHTML = `${string}`
+        }
+        else {
+            string += value.innerHTML;
+            display.innerHTML = `${string}`
+        }
+    })
+})
+
+OFF.addEventListener('click', function () {
+    ON.style.display = "flex"
+    OFF.style.display = "none"
+    Array.from(buttons).forEach(function(value){
+        value.style.display = "none"
+    })
+})
